2c8047fe224bc1ce7c130e1badd817cf06f0aaa1
Loricen <wlechat@aerolith-models.com>
2026-04-08 17:27:48 +0200

    draggable chatbox

--- 9th_campain/src/components/ChatBox.vue
+++ 9th_campain/src/components/ChatBox.vue
@@ -15,8 +15,8 @@ const input    = ref('')
 const listRef  = ref<HTMLElement | null>(null)
 
 // --- Drag ---
-const posX     = window.screen.width*0.2
-const posY     = window.screen.height*0.7
+const posX     = ref(window.screen.width*0.2)
+const posY     = ref(window.screen.height*0.7)
 const dragging = ref(false)
 let dragOffsetX = 0
 let dragOffsetY = 0
